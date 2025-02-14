c3166a37cdf4ffbfcb379eca9d3bd4394c42246d
FrankSua15 <frankosua@gmail.com>
2025-02-13 21:17:12 -0500

    Se agregaron funcionalidades a la to do app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'todoApp';
+  tasks: { text: string, completed: boolean }[] = [];
+  newTask: string = '';
+  filter: string = 'all';
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push({ text: this.newTask, completed: false });
+      this.newTask = ''; // Limpiar input de entrada
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+
+  clearCompleted() {
+    this.tasks = this.tasks.filter(task => !task.completed);
+  }
+     
+  getFilteredTasks() {
+    if (this.filter === 'active') {
+      return this.tasks.filter(task => !task.completed);
+    } else if (this.filter === 'completed') {
+      return this.tasks.filter(task => task.completed);
+    }
+    return this.tasks; 
+  }
+
+  setFilter(filterType: string) {
+    this.filter = filterType;
+  }
 }
